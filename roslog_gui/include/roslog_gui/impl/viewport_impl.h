5bf37f106089a773930eabf256acb443746bff23
Bhaskara Marthi <bhaskara@gmail.com>
2012-05-09 17:22:29 -0700

    Add roslog_gui package

---
+++ roslog_gui/include/roslog_gui/impl/viewport_impl.h
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \file 
+ * 
+ * Implementation of viewport.h
+ *
+ * \author Bhaskara Marthi
+ */
+
+#ifndef ROSLOG_GUI_VIEWPORT_IMPL_H
+#define ROSLOG_GUI_VIEWPORT_IMPL_H
+
+#include <iostream>
+
+namespace roslog_gui
+{
+
+template <typename Item>
+Viewport<Item>::Viewport (const ItemVec& items, const size_t size,
+                          const size_t start) :
+  items_(items), start_(start), size_(size)
+{
+  verifyInvariants();
+}
+
+template <typename Item>
+void Viewport<Item>::verifyInvariants()
+{
+  assert(size_>0);
+  assert(start_<items_.size() || (start_==0 && items_.empty()));
+}
+
+template <typename Item>
+Rows Viewport<Item>::scroll (const int num_rows)
+{
+  if (num_rows < 0)
+  {
+    const size_t nr = -num_rows;
+    if (nr>start_)
+      start_ = 0;
+    else
+      start_ -= nr;
+  }
+  else if (items_.size()>0)
+  {
+    const size_t last = items_.size()-1;
+    start_ = std::min(start_+num_rows, last);
+  }
+  verifyInvariants();
+  return Rows(0, size_);
+}
+
+template <typename Item>
+Rows Viewport<Item>::postpend (const ItemVec& new_items)
+{
+  const size_t prev_size = items_.size();
+  const size_t last_visible = start_ + size_ - 1;
+  items_.insert(items_.end(), new_items.begin(), new_items.end());
+  const size_t new_size = items_.size();
+  
+  if (prev_size == 0)
+    return Rows(0, size_);
+  
+  // If we can currently see the last item
+  else if (prev_size-1 <= last_visible)
+  {
+    // Special case logic to deal with case when we're at the end and 
+    // need to scroll up as new items come in
+    if (new_size-1 > last_visible)
+    {
+      start_ = new_size - size_;
+      verifyInvariants();
+      return Rows(0, size_);
+    }
+    else
+    {
+      return Rows(prev_size-start_, new_size-start_);
+    }
+  }
+  // If nothing changes in visible region
+  else
+    return Rows(0, 0);
+}
+
+// Just insert new items; visible region guaranteed not to change
+template <typename Item>
+Rows Viewport<Item>::prepend (const ItemVec& new_items)
+{
+  ItemVec items = new_items;
+  items.insert(items.end(), items_.begin(), items_.end());
+  items_.swap(items);
+  verifyInvariants();
+  return Rows(0, 0);
+}
+
+// Change size and redisplay everything
+template <typename Item>
+Rows Viewport<Item>::resize (const size_t new_size)
+{
+  assert(new_size>0);
+  size_ = new_size;
+  verifyInvariants();
+  return Rows(0, size_);
+}
+
+// Update the item set, then figure out where within the new set our
+// viewport should be; we do this by getting a real-valued 'stamp'
+// from the current first item, then looking for that stamp among
+// the new items
+template <typename Item>
+Rows Viewport<Item>::updateItems (const ItemVec& new_items)
+{
+  const double stamp = (items_.size()>0) ? getStamp(items_[0]) : 0.0;
+  start_ = findItem(new_items, stamp);
+  items_ = new_items;
+  verifyInvariants();
+  return Rows(0, size_);
+}
+
+// Check that none of the removed items are visible, then remove them 
+template <typename Item>
+void Viewport<Item>::removeFromEnd (const size_t n)
+{
+  const size_t m = items_.size();
+  assert(m>n);
+  assert(m-n>=start_+size_);
+  items_.erase(items_.begin()+m-n, items_.end());
+  verifyInvariants();
+}
+
+// Check that removed items aren't visible, remove them and update
+template <typename Item>
+void Viewport<Item>::removeFromBeginning (const size_t n)
+{
+  assert(n<items_.size());
+  assert(n<start_);
+  ItemVec new_items(items_.begin()+n, items_.end());
+  items_.swap(new_items);
+  start_ -= n;
+  verifyInvariants();
+}
+
+// Debug
+template <typename Item>
+void Viewport<Item>::print ()
+{
+  std::cerr << "[Viewport on range [" << start_ << ", " << start_+size_ <<
+    ") among " << items_.size() << " items]\n";
+}
+
+
+} // namespace
+
+#endif // include guard
---
+++ roslog_gui/include/roslog_gui/viewport.h
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \file 
+ * 
+ * Defines viewport class
+ *
+ * \author Bhaskara Marthi
+ */
+
+#ifndef ROSLOG_GUI_VIEWPORT_H
+#define ROSLOG_GUI_VIEWPORT_H
+
+#include <boost/optional.hpp>
+#include <vector>
+#include <ostream>
+
+namespace roslog_gui
+{
+
+typedef std::pair<size_t, size_t> Rows;
+
+// Viewport maintains state of a window that is displaying a set of rows
+// from some larger set of items.  You can do various operations to
+// scroll the viewport and change the underlying item set.  All of them
+// return the range of rows (relative to the viewport) that must now be
+// redisplayed.
+template <typename Item>
+class Viewport
+{
+public:
+
+  typedef std::vector<Item> ItemVec;
+
+  // Constructor
+  // size must be positive, and start < num items
+  Viewport (const ItemVec& items, const size_t size, const size_t start=0);
+
+  // Add some new items to the end of the set
+  Rows postpend (const ItemVec& new_items);
+
+  // Add some new items at the beginning
+  Rows prepend (const ItemVec& new_items);
+  
+  // Remove items at end
+  void removeFromEnd (size_t n);
+
+  // Remove items at beginning
+  void removeFromBeginning (size_t n);
+
+  // Update item set
+  Rows updateItems (const ItemVec& new_items);
+  
+  // Scroll the viewport by n lines (negative means scroll down)
+  Rows scroll (int n);
+  
+  // Resize the viewport (new size must be positive)
+  Rows resize (size_t new_size);
+  
+  // Get item displayed on row I, or empty if there's no item
+  boost::optional<Item> itemOnRow (size_t r);
+
+  // Debug
+  void print ();
+  
+private:
+  
+  // Debugging method to check consistency
+  void verifyInvariants();
+
+  // Current item set
+  ItemVec items_;
+  
+  // The first item that is being displayed, so between 0 and items_.size()-1,
+  // except can be equal to items.size() if items is empty
+  size_t start_;
+  
+  // The number of items being displayed
+  size_t size_;
+};
+
+} // namespace
+
+#include "impl/viewport_impl.h"
+
+#endif // include guard
