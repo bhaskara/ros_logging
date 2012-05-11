e57a13bb846e8017e3c8ca179135f5ca4e58d68d
Bhaskara Marthi <bhaskara@gmail.com>
2012-05-11 14:52:11 -0700

    Lazy table model update example

--- roslog_gui/include/roslog_gui/rosout_gui.h
+++ roslog_gui/include/roslog_gui/rosout_gui.h
@@ -1,11 +1,49 @@
 #include <QtGui>
 
+class DbModel : public QAbstractTableModel
+{
+  Q_OBJECT
+
+public:
+  DbModel (QObject* parent=0);
+
+  void setRow(int i);
+  void maybeUpdate();
+
+private:
+
+  int rowCount (const QModelIndex& parent) const;
+  int columnCount (const QModelIndex& parent) const;
+  QVariant data (const QModelIndex& ind, int role=Qt::DisplayRole) const;
+  QVariant headerData (int section, Qt::Orientation orientation,
+                       int role=Qt::DisplayRole) const;
+  void postpend (int a, int b);
+  
+  std::map<int, int> squares_;
+  int row_;
+  
+};
+
+class ModelUpdateThread : public QThread
+{
+  Q_OBJECT
+
+public:
+  ModelUpdateThread (DbModel* model) : model_(model) {}
+  void run ();
+
+private:
+
+  DbModel* model_;
+};
+
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
 public:
   MainWindow ();
+               
   
 private slots:
   void itemsScrolled(int i);
@@ -14,5 +52,7 @@ private:
   
   void createStatusBar();
 
-  QTableView *items_;
+  QTableView* items_;
+  DbModel* model_;
+  ModelUpdateThread* updater_;
 };
--- roslog_gui/src/rosout_gui.cpp
+++ roslog_gui/src/rosout_gui.cpp
@@ -13,14 +13,103 @@ QString qstring (const format& f)
   return QString(f.str().c_str());
 }
 
-MainWindow::MainWindow()
+int DbModel::rowCount (const QModelIndex& parent) const
+{
+  return squares_.size();
+}
+
+int DbModel::columnCount (const QModelIndex& parent) const
+{
+  return 2;
+}
+
+QVariant DbModel::headerData (int section, Qt::Orientation orientation,
+                              int role) const
+{
+  if (role!=Qt::DisplayRole)
+    return QVariant();
+  if (orientation==Qt::Horizontal)
+  {
+  if (section==0)
+    return QString("Number");
+  else
+    return QString("Square");
+  }
+  else
+  {
+    return section;
+  }
+}
+
+QVariant DbModel::data (const QModelIndex& ind, int role) const
+{
+  const int r = ind.row();
+  if (role!=Qt::DisplayRole) // Invalid role
+    return QVariant();
+  else if (!ind.isValid())
+    return QVariant();
+  else if (r>=(int)squares_.size())
+    return QVariant();
+  else
+  {
+    std::map<int, int>::const_iterator e = squares_.begin();
+    advance(e, r);
+    return ind.column() ? e->second : e->first;
+  }
+}
+
+DbModel::DbModel (QObject* parent) :
+  QAbstractTableModel(parent), row_(0)
+{
+  postpend(0, 50);
+}
+
+void DbModel::postpend (const int a, const int b)
+{
+  const int nr = rowCount(QModelIndex());
+  beginInsertRows(QModelIndex(), nr, nr+b-a);
+  for (int i=a; i<b; i++)
+    squares_[i]=i*i;
+  endInsertRows();
+}
+
+void DbModel::setRow (const int r)
+{
+  row_ = r;
+}
+
+void DbModel::maybeUpdate ()
+{
+  int s = squares_.size();
+  if (row_+20 > s)
+  {
+    cerr << "Updating to max row " << s+20 << endl;
+    postpend(s, s+20);
+  }
+}
+
+
+
+void ModelUpdateThread::run()
+{
+  while (true)
+  {
+    QThread::msleep(100.0);
+    if (model_)
+      model_->maybeUpdate();
+  }
+}
+  
+  
+MainWindow::MainWindow() 
 {
   items_ = new QTableView;
   setCentralWidget(items_);
   createStatusBar();
-  
-  for (unsigned i=0; i<100; i++)
-    items_->addItem(QString(boost::lexical_cast<string>(i).c_str()));
+  model_ = new DbModel;
+  items_->setModel(model_);
+  updater_ = new ModelUpdateThread(model_);
+  updater_->start();
   
   connect(items_->verticalScrollBar(), SIGNAL(valueChanged(int)), this,
           SLOT(itemsScrolled(int)));
@@ -31,6 +120,7 @@ void MainWindow::itemsScrolled (int i)
 {
   format f("Scrolled to %1%");
   statusBar()->showMessage(qstring(f % i));
+  model_->setRow(i);
 }
 
 void MainWindow::createStatusBar()
